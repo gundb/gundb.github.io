81f0e25036137221d030a52ba925a93fe4090946
Robert Heessels <robert@heessels.com>
2018-02-06 12:48:23 +0100

    Switched to loading navigation from repo instead of wiki.

--- docs/src/src/app.vue
+++ docs/src/src/app.vue
@@ -35,11 +35,11 @@
 
             <h3>Documentation</h3>
 
-            <ul class="uk-nav uk-nav-default tm-nav" :class="{ 'uk-margin-top': index }" v-for="(item, index) in navigation">
-                <li class="qqquk-nav-header gn-menu-section">{{item.title}}</li>
-                <router-link class="gn-indent" tag="li" :to="itm.ky" :key="itm.ky" v-for="(itm) in item.pages" qqqexact>
-                  <a>{{itm.vl}}</a>
-                  <ul class="uk-nav uk-nav-default tm-nav gn-indent" v-if="$route.params.page === decodeURIComponent(itm.ky)">
+            <ul class="uk-nav uk-nav-default tm-nav" :class="{ 'uk-margin-top': index }" v-for="(pages, category, index) in navigation">
+                <li class="qqquk-nav-header gn-menu-section">{{category}}</li>
+                <router-link class="gn-indent" tag="li" :to="p" :key="p" v-for="(p, label) in pages" qqqexact>
+                  <a>{{label}}</a>
+                  <ul class="uk-nav uk-nav-default tm-nav gn-indent" v-if="$route.params.page === decodeURIComponent(p)">
                     <li class="gn-menu-bookmark-link" v-for="(id, subject) in ids">
                         <a :href="'#'+id">{{ subject }}</a>
                     </li>
@@ -106,10 +106,10 @@
                         <li><a href="../download">Download</a></li>
                     </ul> -->
 
-                    <ul class="uk-nav uk-nav-default tm-nav uk-margin-top" v-for="(item, index) in navigation">
-                        <li class="uk-nav-header">{{item.title}}</li>
-                        <li v-for="(itm) in item.pages" exact>
-                          <a :href="'./'+itm.ky">{{itm.vl}}</a>
+                    <ul class="uk-nav uk-nav-default tm-nav uk-margin-top" v-for="(pages, category, index) in navigation">
+                        <li class="uk-nav-header">{{category}}</li>
+                        <li v-for="(p, label) in pages" exact>
+                          <a :href="'./'+p">{{label}}</a>
                         </li>
                     </ul>
                 </div>
@@ -127,11 +127,10 @@ export default {
   name: "app",
   data() {
     return {
-      navigation: [], // navigation
+      navigation: {},
       ids: {},
       page: false,
       // component: false
-      cache: {},
       pageTitle: ''
     }
   },
@@ -146,10 +145,10 @@ export default {
 
   methods: {
     setPageTitle(urlPart) {
-      for(let item of this.navigation) {
-        for(let itm of item.pages) {
-          if (urlPart === decodeURIComponent(itm.ky)) {
-            this.pageTitle = itm.vl
+      for(let item in this.navigation) {
+        for(let itm in this.navigation[item]) {
+          if (urlPart === decodeURIComponent(this.navigation[item][itm])) {
+            this.pageTitle = itm
           }
         }
       }
@@ -159,51 +158,15 @@ export default {
   mounted() {
     // UIkit.offcanvas('#offcanvas', {}).show()
 
-    var page = '_MENU_'
     new Promise((resolve, reject) => {
-      if (this.cache[page]) {
-        resolve(this.cache[page]);
-        return;
-      }
-
-      ajax(`https://raw.githubusercontent.com/wiki/amark/gun/${page}.md?nc=${Math.random()}`).then(
-        ({ response }) => {
-          if (startsWith(response.trim(), "<!DOCTYPE html>")) {
-            response = ``;
-          }
-          this.cache[page] = response;
-          resolve(response);
-        },
-        err => reject(err)
-      );
-    }).then(page => {
-      var menuItems = page.split('\n')
-      let nav = []
-      let l1i = -1
-      for(let item of menuItems) {
-        let level = 0
-        if (item.substr(0, 2) === '# ') {
-          level = 1
-        }
-        if (item.substr(0, 3) === '## ') {
-          level = 2
-        }
-        if (level > 0) {
-          let s = item.substr(level + 1)
-          if (level === 1) {
-            l1i++
-            nav.push({title: s, pages: []})
-          } else {
-            let [vl, ky] = s.split(/:(.+)/)
-            ky = ky.trim()
-            nav[l1i].pages.push({ky: ky, vl: vl})
-          }
-        }
-      }
-      this.navigation = nav
-      this.setPageTitle(this.$route.params.page)
-    }, () => (this.error = "Failed loading page"));
-
+      ajax(`http://gun.js.org/docs/navigation.json?nc=${Math.random()}`).then(page => {
+        try {
+          let json = JSON.parse(page.response)
+          this.navigation = json
+          this.setPageTitle(this.$route.params.page)
+        } catch(e) {}
+      })
+    })
   }
-};
+}
 </script>
